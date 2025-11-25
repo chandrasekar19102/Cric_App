2d5af0750248227f38fe83aea8274b5c9e621ccd
Naveenraj <naveenraj00154@gmail.com>
2025-11-26 00:21:39 +0530

    created dummy models

--- db model.sql
+++ db model.sql
@@ -1,5 +1,9 @@
 Organizer 
+<<<<<<< HEAD
 	id INTEGER PRIMARY KEY 	
+=======
+	id INTEGER PRIMARY KEY 
+>>>>>>> main
 	name STRING
 
 Tournament
@@ -26,6 +30,7 @@ Matches
 	Tournament_id INTEGER FOREIGN KEY 
 	team_a_id INTEGER FOREIGN KEY 
 	team_b_id INTEGER FOREIGN KEY
+<<<<<<< HEAD
 	toss_winning_team INTEGER NULL FOREIGN KEY REFERENCES Team(id)
 	winning_team INTEGER NULL FOREIGN KEY REFERENCES team(id)
 	won_by_runs INTEGER  
@@ -37,10 +42,22 @@ Inning
 	match_id INTEGER FOREIGN KEY matches(id)
 	innings INTEGER {first_innings , second_innings}
 	innings_type STRING
+=======
+	toss_winner_team_id INTEGER NULL FOREIGN KEY REFERENCES Team(id)
+	winner_team_id INTEGER NULL FOREIGN KEY REFERENCES team(id)
+	win_by_runs INTEGER  
+	CONSTRAINT team_a_b_are_not_equal CHECK (team_a_id <> team_b_id)
+	
+Inning
+	id
+	match_id INTEGER FOREIGN KEY matches(id)
+	Innings STRING {first_innings , second_innings}
+>>>>>>> main
 	batting_team INTEGER FOREIGN KEY 
 	bowling_team INTEGER FOREIGN key 
 	
 	
+<<<<<<< HEAD
 Overs
 	id INTEGER PRIMARY KEY
 	inning_id INTEGER FOREIGN KEY inning(id)
@@ -50,18 +67,37 @@ Overs
 	
 ball
 	id  INTEGER PRIMARY KEY
+=======
+over
+	id
+	inning_id INTEGER FOREIGN KEY inning(id)
+	over_number
+	bowler_id
+	
+ball
+	id 
+>>>>>>> main
 	over_id INTEGER FOREIGN KEY over(id)
 	ball_number
 	striker_id
 	non_striker_id
 	bowler_id
+<<<<<<< HEAD
 	batter_run
 	extra_runs
+=======
+	runs_batsman
+	runs_extras
+>>>>>>> main
 	extra_type
 	is_wicket
 	witcket_type(bowled,catch,runout,lbw)
 	dismissed_player_id
 	
+<<<<<<< HEAD
 
+=======
+	
+>>>>>>> main
 	
 	
